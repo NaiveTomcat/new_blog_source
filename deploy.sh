a399e9ad6e31ca78abe1ed7d2c87db1713177349
NaiveTomcat <tomdang@naivetomcat.cn>
2023-01-30 16:25:08 +0800

    using rsync to publish

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 yarn build
-scp -r docs/.vuepress/dist/* root@lax.tsinghuamakerxian.cn:/var/www/blog/
+#scp -r docs/.vuepress/dist/* root@lax.tsinghuamakerxian.cn:/var/www/blog/
+rsync -rz --progress docs/.vuepress/dist/ root@lax.tsinghuamakerxian.cn:/var/www/blog
 yarn baiduPush
 curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://blog.tsinghuamakerxian.cn&token=85APzPfi9kHnXxmf"
 # cp -r docs/.vuepress/dist/* gitpublish/
